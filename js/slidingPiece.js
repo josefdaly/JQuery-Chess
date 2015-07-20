4d4e842df026037c4b06cd05d0ad1c0115836ae3
Your Name <josefdaly@gmail.com>
2015-07-20 11:54:34 -0700

    Fix container side

--- js/slidingPiece.js
+++ js/slidingPiece.js
@@ -16,13 +16,12 @@
   }
 
   SlidingPiece.prototype.explore = function (dir) {
-    pos = [this.pos[0], this.pos[1]]
-    positions = []
-    debugger
+    var pos = [parseInt(this.pos[0]), parseInt(this.pos[1])]
+    var positions = []
 
-    while (this.board.onBoard([parseInt(pos[0]) + dir[0], parseInt(pos[1]) + dir[1]])) {
-      pos[0] = dir[0] + parseInt(pos[0])
-      pos[1] = dir[1] + parseInt(pos[1])
+    while (this.board.onBoard([pos[0] + dir[0], pos[1] + dir[1]])) {
+      pos[0] = dir[0] + pos[0]
+      pos[1] = dir[1] + pos[1]
       if (this.board.occupied(pos)) {
         if (this.board.pieceAt(pos).color === this.color) {
           break
